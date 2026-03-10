f05172f0d17714004bd0879ad2785e759ebb11d9
Bastab10 <bastabsaikiabs10@gmail.com>
2026-03-10 14:19:01 +0530

    okk

--- models/User.js
+++ models/User.js
@@ -29,7 +29,8 @@ const userSchema = new mongoose.Schema({
         },
         lastName: {
             type: String,
-            required: true
+            required: false,
+            default: ''
         },
         fullName: {
             type: String,
